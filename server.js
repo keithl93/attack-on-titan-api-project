6703e2647df3322e98ba8f4b7559d267291716e8
Keith Lee <keithfromseoul@gmail.com>
2023-07-21 14:15:44 -0400

    debug commit

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import db from './lib/db/connection.js'
 import express from 'express'
+import routes from './lib/routes/index.js'
 
 const app = express();
 const port = 3000;
 
-//set the view engine to ejs
-app.set('view engine', 'ejs');
+app.use('/', routes);
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+//set the view engine to ejs
+// app.set('view engine', 'ejs');
+
+// app.get('/', function (req, res) {
+//   res.render('index');
+// });
+
 
 
 app.listen(port);

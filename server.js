a4d8a9f950dcad8144c2505d53709ff5aff5204a
Keith Lee <keithfromseoul@gmail.com>
2023-07-22 22:06:13 -0400

    connection to index.ejs

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 import db from './lib/db/connection.js'
 import express from 'express'
 import routes from './lib/routes/index.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'lib', 'views'))); 
 app.use('/', routes);
 
-//set the view engine to ejs
-// app.set('view engine', 'ejs');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'lib', 'views')); 
 
-// app.get('/', function (req, res) {
-//   res.render('index');
-// });
-
-
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
 app.listen(port);
 console.log(`Server running on ${port}`);

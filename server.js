75fcb50d3b709371a4f2097b38ca71b2a13e5f9d
Keith Lee <keithfromseoul@gmail.com>
2023-07-24 15:48:49 -0400

    update port'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import routes from './lib/routes/index.js'
 // const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public'))); 

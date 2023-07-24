af2657f7d44f9a23424eb7490ffa18446b1d534b
Keith Lee <keithfromseoul@gmail.com>
2023-07-23 23:02:36 -0400

    front end commit

--- server.js
+++ server.js
@@ -12,16 +12,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'lib', 'views'))); 
+app.use(express.static(path.join(__dirname, 'public'))); // 정적 폴더로 public 폴더를 설정
 app.use('/', routes);
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'lib', 'views')); 
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
 
 app.listen(port);
 console.log(`Server running on ${port}`);

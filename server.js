c3ef44eb38a0845ba97666c39bb74f7f08c21bd1
Keith Lee <keithfromseoul@gmail.com>
2023-07-24 15:17:40 -0400

    heroku setup

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 import db from './lib/db/connection.js'
 import express from 'express'
 import routes from './lib/routes/index.js'
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // 정적 폴더로 public 폴더를 설정
+// app.use(express.static(path.join(__dirname, 'public'))); 
 app.use('/', routes);
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 app.listen(port);
 console.log(`Server running on ${port}`);

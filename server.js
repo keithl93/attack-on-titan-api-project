191157955ba9e92699c7341233e51a5367ef96b0
Keith Lee <keithfromseoul@gmail.com>
2023-07-24 16:51:15 -0400

    formatted prettier

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import db from './lib/db/connection.js'
-import express from 'express'
-import routes from './lib/routes/index.js'
+import db from "./lib/db/connection.js";
+import express from "express";
+import routes from "./lib/routes/index.js";
 // import path from 'path';
 // import { fileURLToPath } from 'url';
 // import { dirname } from 'path';
@@ -9,11 +9,11 @@ import routes from './lib/routes/index.js'
 // const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public'))); 
-app.use('/', routes);
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", routes);
 
 // set the view engine to ejs
 // app.set('view engine', 'ejs');

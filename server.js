aadd7dbca3e4df202788198b90d7b6ff8f555d39
Keith Lee <keithfromseoul@gmail.com>
2023-07-25 09:39:55 -0400

    update cors and id

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import db from "./lib/db/connection.js";
 import express from "express";
 import routes from "./lib/routes/index.js";
+import cors from "cors";
+
 // import path from 'path';
 // import { fileURLToPath } from 'url';
 // import { dirname } from 'path';
@@ -9,10 +11,12 @@ import routes from "./lib/routes/index.js";
 // const __dirname = dirname(__filename);
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 app.use("/", routes);
 
 // set the view engine to ejs
